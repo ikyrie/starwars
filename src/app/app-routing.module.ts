4fa7b97782f60ef5890310b8d1601e0f47b4dab4
ikyrie <mah.c5r@gmail.com>
2019-09-25 14:05:34 -0300

    Criando as rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PeopleComponent } from './components/people/people.component';
+import { MovieComponent } from './components/movie/movie.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PeopleComponent },
+  { path: 'movie', component: MovieComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PeopleComponent } from './components/people/people.component';
+import { PersonComponent } from './components/person/person.component';
+import { MovieComponent } from './components/movie/movie.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PeopleComponent
+    PeopleComponent,
+    PersonComponent,
+    MovieComponent
   ],
   imports: [
     BrowserModule,
